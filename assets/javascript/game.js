910ba152c6024d11af31aeda56b99897649855aa
dan <danbrianlewis@gmail.com>
2017-12-07 18:33:56 -0500

    finished to submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let towns =["MANALAPAN","FREEHOLD", "ASBURY", "HOWELL","GLASSBORO","HOBOKEN"];
+let towns =["MANALAPAN","FREEHOLD", "ASBURY", "HOWELL","GLASSBORO","HOBOKEN","TRENTON","NEWARK","CAMDEN","JACKSON"];
 let townToGuess;
-let guessedLetters = [];
+let alreadyGuessedLetters = [];
 let guessesLeftCounter;
 let guessesCorrectCounter;
 let currentGuessed =[];
 
-
 const currentWordText = document.getElementById("currentWordStat");
-const guessedLettersText = document.getElementById("lettersGuess");
+const alreadyGuessedLettersText = document.getElementById("lettersGuess");
 const guessesLeftText = document.getElementById("guessesLeft");
 const messageText = document.getElementById("message");
 
@@ -21,21 +20,17 @@ function printScreen(){
 	console.log("currentGuessed")
 }
 
-function messageAlert(str){
-	messageText.textContent = str
-}
-
 function printLetter(ltr){
 	console.log(ltr)
-	for(i = 0; i< townToGuess.length; i++){
+	for(let i = 0; i< townToGuess.length; i++){
 	        if(ltr == townToGuess[i]){
 	        	currentGuessed[i] = ltr;
 	        	guessesCorrectCounter++;
-	        	console.log(guessesCorrectCounter + "Correct")
+	        	console.log(guessesCorrectCounter + " letters correct out of "+ townToGuess.length)
 	        	messageAlert("Correct");
 	        	endGame()
 	        }else if(currentGuessed[i] !== "_"){
-	        	console.log(null);
+	        	console.log("null for a reason");
 	        }else{
 	        	currentGuessed[i] = "_"
 	        }
@@ -47,12 +42,12 @@ function printLetter(ltr){
 function start(){
 	townToGuess = towns[Math.floor(Math.random() * towns.length)];
 	townToGuess = townToGuess.split("");
-	guessedLetters = [];
+	alreadyGuessedLetters = [];
 	guessesLeftCounter = 8;
 	guessesCorrectCounter = 0;
 	printScreen();
-	guessedLettersText.textContent = guessedLetters;
-	guessesLeftText.textContent = guessesLeftCounter;
+	updateStats();
+	messageAlert("Start by selecting a letter.")
 	console.log(townToGuess);
 
 }
@@ -60,38 +55,57 @@ function start(){
 document.onload = start();
 
 
-
-
 document.onkeydown = function(event){
 	let userGuess = event.key
 	userGuess = userGuess.toUpperCase();
 	if(letters.indexOf(userGuess) == -1){
-		messageAlert("Please choose a letter")
-	}else if(townToGuess.indexOf(userGuess) == -1){
-		guessedLetters.push(userGuess);
+		messageAlert("Please choose a letter.")
+	}else if(alreadyGuessed(userGuess) == true){
+		messageAlert("Whats the matter? You already tried that!");
+		updateStats();
+	}else if(testCorrect(userGuess) == false){
+		alreadyGuessedLetters.push(userGuess);
 		guessesLeftCounter--;
-		guessesLeftText.textContent = guessesLeftCounter;
-		guessedLettersText.textContent = guessedLetters;
-		endGame();
+		updateStats();
 		messageAlert("Try Again.");
-	}else if(currentGuessed.indexOf(userGuess) !== -1){
-		messageAlert("Whats the matter? You already tried that!")
-		guessedLettersText.textContent = guessedLetters
-	}else{
+		endGame();
+	}else if(testCorrect(userGuess)==true){
 		printLetter(userGuess);
-	    }
+		console.log(userGuess + " was correct")
+	}else{
+		console.log("uhhh something went wrong")
 	}
+}
+
+function updateStats(){
+	guessesLeftText.textContent = guessesLeftCounter;
+	alreadyGuessedLettersText.textContent = alreadyGuessedLetters;
+}
+
+function messageAlert(str){
+	messageText.textContent = str;
+}
+
+function testCorrect(gCurrent){
+	return townToGuess.includes(gCurrent);
+}
+
+function alreadyGuessed(gCurrent){
+	return alreadyGuessedLetters.includes(gCurrent);
+}
 
 
 function endGame(){
 	if(guessesLeftCounter == 0){
-		messageAlert("Get out of here!");
+		messageAlert("Get lost!");
 		start();
 	}else if(guessesCorrectCounter == townToGuess.length){
-		messageAlert("You can sit with us...for now.");
+		messageAlert("Bruce would be proud.");
 		start();
 	}else{
 		console.log("no winner yet")
 	}
 
 }
+
+
